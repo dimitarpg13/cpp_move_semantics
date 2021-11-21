3acc63f0077cfe6f96390b55ec7810f0654b65e6
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2021-11-21 18:50:02 -0500

    example update

---
+++ basics/customerimpl.cpp
@@ -0,0 +1,19 @@
+#include "customerimpl.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+  std::vector<Customer> coll;
+  for (int i=0; i<12; ++i) {
+    coll.push_back(Customer{"TestCustomer " + std::to_string(i-5)});
+  }
+
+  std::cout << "---- sort():\n";
+  std::sort(coll.begin(), coll.end(),
+            [] (const Customer& c1, const Customer& c2) {
+              return c1.getName() < c2.getName();
+            });
+}
